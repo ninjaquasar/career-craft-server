862d16f96e0b18daacfbb25a034c996885c97ab8
mahnaf <mahnaf125@gmail.com>
2025-05-28 22:55:27 +0600

    chore: setup mongodb and atlas database

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const dotenv = require("dotenv").config;
 const port = 5100;
 const app = express();
+dotenv();
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
+
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@professorcluster.rlegbqz.mongodb.net/?retryWrites=true&w=majority&appName=ProfessorCluster`;
+
+// Create MongoClient
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+// Database Run Script
+async function run() {
+	try {
+		// Connect client to database server
+		await client.connect();
+		// Get/Create Database
+		const database = client.db("career_craft");
+		// Send ping for successful connection confirmation
+		await client.db("admin").command({ ping: 1 });
+		console.log("Successfully connected to Database.");
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 // Home page response
 app.get("/", (req, res) => {
